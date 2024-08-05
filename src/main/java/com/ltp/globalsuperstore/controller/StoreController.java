ab8f09b0b1d003d899a07f3b78dd7ed097d185ae
Kris Haroun <kris.haroun@outlook.com>
2024-08-05 18:29:31 +0100

    re-organised code into three layer codebase model

--- src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
+++ src/main/java/com/ltp/globalsuperstore/controller/StoreController.java
@@ -1,9 +1,4 @@
-package com.ltp.globalsuperstore;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+package com.ltp.globalsuperstore.controller;
 
 import javax.validation.Valid;
 
@@ -13,18 +8,21 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ltp.globalsuperstore.Item;
+import com.ltp.globalsuperstore.StoreService;
+
 import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class StoreController {
-    
-    List<Item> items = new ArrayList<>();
-    
+
+    StoreService storeService = new StoreService();
+        
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id) {
-        int index = getIndexFromID(id);
-        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
+        model.addAttribute("item", storeService.getItemFromId(id));
         return "form";
     }
 
@@ -36,16 +34,7 @@ public class StoreController {
             }
         }
         if (result.hasErrors()) return "form";
-        int index = getIndexFromID(item.getId());
-        String status = Constants.SUCCESS_STATUS;
-        if (index == Constants.NOT_FOUND) {
-            items.add(item);
-        } else if (within5Days(item.getDate(), items.get(index).getDate())){
-            items.set(index, item);
-        } else {
-            status = Constants.FAILED_STATUS;
-        }
-
+        String status = storeService.handleSubmit(item);
         redirectAttributes.addFlashAttribute("status", status);
 
         return "redirect:/inventory";
@@ -54,21 +43,8 @@ public class StoreController {
     
     @GetMapping("/inventory")
     public String getInventory(Model model) {
-        model.addAttribute("items", items);
+        model.addAttribute("items", storeService.getItems());
         return "inventory";
     }
-
-    public int getIndexFromID(String id) {
-        for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
-
-    public boolean within5Days(Date newDate, Date oldDate) {
-        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-    }
-
-    
+ 
 }
